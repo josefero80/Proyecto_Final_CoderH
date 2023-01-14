9a5a5ee763b37e92b8368ba923dbe204d17d081a
Jose Ojeda <josefero80@gmail.com>
2023-01-13 20:09:54 -0500

    completo blogs

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from blog.views import docu1, docu2, docu3
+from blog.views import docu1, docu2, docu3, docu4, docu5
 
 urlpatterns = [
     path('docu1/', docu1, name="docu1"),
     path('docu2/', docu2, name="docu2"),
     path('docu3/', docu3, name="docu3"),
+    path('docu4/', docu4, name="docu4"),
+    path('docu5/', docu5, name="docu5"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -15,3 +15,9 @@ def docu2(request):
 
 def docu3(request):
     return render(request, 'blog/docu3.html',)
+
+def docu4(request):
+    return render(request, 'blog/docu4.html',)
+
+def docu5(request):
+    return render(request, 'blog/docu5.html',)
